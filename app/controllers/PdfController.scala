fc9a37bf4923506cffc9a172ee33a2a7d64c4945
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2020-12-08 09:07:07 +0000

    TRUS-3597: Increase coverage minimum to 80% and exclude routes files

--- app/controllers/PdfController.scala
+++ app/controllers/PdfController.scala
@@ -28,11 +28,13 @@ import play.api.libs.json.Json
 import play.api.mvc._
 import utils.PdfFileNameGenerator
 
+import scala.concurrent.ExecutionContext
+
 class PdfController @Inject()(action: DefaultActionBuilder,
                               nrsConnector: NrsConnector,
                               config: AppConfig) extends Logging {
 
-  implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
+  implicit val ec: ExecutionContext = ExecutionContext.global
 
   def getPdf(): Action[AnyContent] = action.async {
     implicit request =>
