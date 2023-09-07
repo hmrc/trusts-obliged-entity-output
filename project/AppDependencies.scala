8ce939eae3ac312d6c0161b3bd134935dd8a9b82
Paul Callaghan <83694362+PaulC-TDS@users.noreply.github.com>
2023-09-07 16:15:08 +0100

    DDCE-4530 PR feedback

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -17,13 +17,13 @@ object AppDependencies {
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo"         %% "hmrc-mongo-test-play-28"  % mongoHmrcVersion,
     "uk.gov.hmrc"               %% "bootstrap-test-play-28"   % bootstrapVersion,
-    "org.scalatest"             %% "scalatest"                % "3.2.16",
+    "org.scalatest"             %% "scalatest"                % "3.2.17",
     "com.typesafe.play"         %% "play-test"                % current,
     "org.scalatestplus.play"    %% "scalatestplus-play"       % "5.1.0",
     "org.scalacheck"            %% "scalacheck"               % "1.17.0",
     "com.vladsch.flexmark"      % "flexmark-all"              % "0.64.8",
     "org.mockito"               % "mockito-core"              % "5.5.0",
-    "org.wiremock" % "wiremock-standalone" % "3.0.1"
+    "org.wiremock"              % "wiremock-standalone"       % "3.0.1"
   ).map(_ % "test, it")
 
 }
