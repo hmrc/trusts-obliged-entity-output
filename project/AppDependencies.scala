6a2704cfc98b44c8a4c40c1f951eac300ad9141b
shridhar-chippa <189346280+shridhar-chippa@users.noreply.github.com>
2025-05-12 15:40:36 +0100

    DDCE-6677: Upgrade libraries

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,9 +4,9 @@ import sbt._
 
 object AppDependencies {
 
-  private lazy val mongoHmrcVersion = "1.9.0"
+  private lazy val mongoHmrcVersion = "2.6.0"
 
-  private val bootstrapVersion = "9.5.0"
+  private val bootstrapVersion = "9.11.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                 %% "bootstrap-backend-play-30"  % bootstrapVersion,
@@ -18,13 +18,13 @@ object AppDependencies {
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo"         %% "hmrc-mongo-test-play-30"  % mongoHmrcVersion,
     "uk.gov.hmrc"               %% "bootstrap-test-play-30"   % bootstrapVersion,
-    "org.scalatest"             %% "scalatest"                % "3.2.18",
+    "org.scalatest"             %% "scalatest"                % "3.2.19",
     "org.scalatestplus.play"    %% "scalatestplus-play"       % "7.0.1",
     "org.playframework"         %% "play-test"                % current,
-    "org.scalacheck"            %% "scalacheck"               % "1.18.0",
+    "org.scalacheck"            %% "scalacheck"               % "1.18.1",
     "com.vladsch.flexmark"      % "flexmark-all"              % "0.64.8",
-    "org.mockito"               % "mockito-core"              % "5.12.0",
-    "org.wiremock"              % "wiremock-standalone"       % "3.5.4"
+    "org.mockito"               % "mockito-core"              % "5.17.0",
+    "org.wiremock"              % "wiremock-standalone"       % "3.13.0"
   ).map(_ % "test, it")
 
 }
